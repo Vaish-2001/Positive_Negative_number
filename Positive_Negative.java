0a09cda748d59f461c45382510ced59c8186730a
vaishnavi thakare <159428762+Vaish-2001@users.noreply.github.com>
2024-02-23 12:34:05 +0530

    Add files via upload

---
+++ Positive_Negative.java
@@ -0,0 +1,10 @@
+class Positive_Negative
+{
+public static void main(String[]args)
+ {
+  int b=8;
+  String result1=(b>=0||b<=0)?("no is positive"):("no is negative");
+  System.out.println(result1);
+ }
+}
+  
